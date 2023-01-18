d4923b916f73d10fa3d78882be36c005dbce9890
zjohnsonhxc <hexcassette@gmail.com>
2023-01-18 11:43:26 -0700

    Complete exercises 1, 2 and 3
    All pieces of cake!

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let output = '';
+    if (num < 0) {
+        return 'ERROR';
+    }
+    for (let i = 0; i < num; i++) {
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
