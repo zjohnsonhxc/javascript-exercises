415e4b4194f7950597fa917fcb431ca2742cf631
zjohnsonhxc <hexcassette@gmail.com>
2023-01-19 18:35:37 -0700

    Complete '04-removefromarray' challenge
    On my first try too! Wasn't sure if my solution was going to work,
    but first try, BOOM.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
 
-};
+    // First for loop runs for as many extra arguments given as parameters
+    for (let i = 1; i < arguments.length; i++) {
+        // 'j' for loop runs for the length of array, iterating over each
+        // item in array and checking if it is equal to that extra argument
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === arguments[i]) {
+                // if they are equal, arr.splice gets rid of that element
+                arr.splice(j, 1);
+            }
+            }
+        }
+    return arr;
+    };
 
 // Do not edit below this line
 module.exports = removeFromArray;
