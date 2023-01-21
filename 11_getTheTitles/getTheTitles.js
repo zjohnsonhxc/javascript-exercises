4ce2a6915a0ab083d354e7b16ecfaf88d038fc31
zjohnsonhxc <hexcassette@gmail.com>
2023-01-21 16:02:20 -0700

    Finish '11_getTheTitles'
    Was surprised this one included dictionaries nested inside of arrays,
    when TOP obviously hadn't talked about them yet. Then I look back and
    see that I was only supposed to complete up to challenge #7 at this
    point in the course. Guess I'm ahead of the game :)

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    let titles = [];
+function getTitle(book) {
+  titles.push(book.title);
+}
 
+books.forEach(getTitle);
+return titles;
 };
 
 // Do not edit below this line
