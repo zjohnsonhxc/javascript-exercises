9ca4e0ee75c4c98a4ebaf03d7f19e3b61abc9d14
zjohnsonhxc <hexcassette@gmail.com>
2023-01-21 12:07:33 -0700

    Complete '05_sumAll' challenge
    Main trip-up here was using NaN() and realizing NaN() coerces some
    strings into integers. Using typeof num1 was a better match.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return "ERROR";
+  }
 
+  if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  }
+
+  let largeInteger = Math.max(num1, num2);
+  let smallInteger = Math.min(num1, num2);
+  let sum = 0;
+
+  do {
+    sum += smallInteger;
+    smallInteger++;
+  } while (smallInteger <= largeInteger);
+
+  return sum;
 };
 
 // Do not edit below this line
