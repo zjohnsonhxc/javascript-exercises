471674fd53ba6afd45c96d280fb0f688bcfaf13e
zjohnsonhxc <hexcassette@gmail.com>
2023-02-01 21:19:21 -0700

    Complete 12_findTheOldest
    First of these that I completed after reviewing object methods. This
    one uses array.sort. Should look at their solutions, because they said
    they could "chain" methods or use "reduce," and I did neither, but my
    solution works!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
+function findTheOldest(array) {
+  const newPeople = array.sort(function (a, b) {
+    if (!a.yearOfDeath) {
+      a.yearOfDeath = new Date().getFullYear();
+    }
 
-};
+    if (!b.yearOfDeath) {
+      b.yearOfDeath = new Date().getFullYear();
+    }
+
+    if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+  return newPeople[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
