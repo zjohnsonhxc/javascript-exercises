a27c178a4e5aa14408a370e56da4f2f8c36e2ce5
zjohnsonhxc <hexcassette@gmail.com>
2023-02-01 21:22:46 -0700

    Add comments to project 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,6 @@
 function findTheOldest(array) {
   const newPeople = array.sort(function (a, b) {
+    // if no death, use today's date to find age
     if (!a.yearOfDeath) {
       a.yearOfDeath = new Date().getFullYear();
     }
@@ -8,12 +9,14 @@ function findTheOldest(array) {
       b.yearOfDeath = new Date().getFullYear();
     }
 
+    // sort the oldest to the top of the array
     if (a.yearOfDeath - a.yearOfBirth < b.yearOfDeath - b.yearOfBirth) {
       return 1;
     } else {
       return -1;
     }
   });
+  // return the first in the array, which is the oldest
   return newPeople[0];
 }
 
